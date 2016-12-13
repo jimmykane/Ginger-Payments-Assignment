9531f4adb49be5b501bbc7416f6aff5a6e6f1dac
Dimitrios Kanellopoulos <jimmykane9@gmail.com>
2016-12-13 18:15:08 +0100

    Add some comments

--- public/app/modules/main/controllers/main.js
+++ public/app/modules/main/controllers/main.js
@@ -2,25 +2,31 @@
 
 angular.module('app.main').controller('mainController', function ($scope, dataService) {
 
+    // Our 2 variables for this module's controller
     $scope.payments = [];
     $scope.paymentFormData = {};
 
+    // The callback call
     $scope.callback = function () {
         dataService.getPaymentsWithCallback(function (data) {
             $scope.payments = data;
         });
     };
 
+    // The promise call
     $scope.promise = function () {
         dataService.getPaymentsWithPromise().then(function (response) {
             $scope.payments = response.data;
         });
     };
 
+    // The payment call
     $scope.addPayment = function () {
         dataService.addPayment($scope.paymentFormData).then(function (response) {
             // Reset the table
             $scope.payments = [];
+            // Reset the form inputs
+            $scope.paymentFormData = {};
         });
     };
 });
